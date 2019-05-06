3309d345305609c9bc515b2833a668a10ec8fbfd
Sanjeev <Sanjeev@192.168.2.14>
2019-05-06 17:51:57 -0400

    window opens

--- src/asteroids/Asteroids.java
+++ src/asteroids/Asteroids.java
@@ -5,17 +5,26 @@
  */
 package asteroids;
 
-/**
- *
- * @author Farhan
- */
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import javax.swing.*;
+
 public class Asteroids {
 
-    /**
-     * @param args the command line arguments
-     */
+    public Asteroids() {
+        intro.setPreferredSize(new Dimension(1100, 700));
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.setResizable(false);
+        frame.setVisible(true);
+        frame.setContentPane(intro);
+        frame.pack();
+    }
+
+    private final JFrame frame = new JFrame("Asteroids");
+    private final JPanel intro = new JPanel(new FlowLayout(FlowLayout.CENTER, 0, 0));
+
     public static void main(String[] args) {
-        // TODO code application logic here
+        Asteroids gui = new Asteroids();
     }
 
 }
