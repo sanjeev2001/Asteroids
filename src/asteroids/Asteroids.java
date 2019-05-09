c308a87dd905be40ab8904be13297f35ceecd7d4
Ottoble <Ottoble@10.10.146.141>
2019-05-09 14:16:34 -0400

    message

--- src/asteroids/Asteroids.java
+++ src/asteroids/Asteroids.java
@@ -22,7 +22,7 @@ public class Asteroids {
 
     private final JFrame frame = new JFrame("Asteroids");
     private final JPanel intro = new JPanel(new FlowLayout(FlowLayout.CENTER, 0, 0));
-    // okaodd
+    // message
 
     public static void main(String[] args) {
         Asteroids gui = new Asteroids();
