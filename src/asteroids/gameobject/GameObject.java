8ec4c40ed5bdefa83709f549f808781de61116a9
Farhan Faruqui <farhanfaruqui13@gmail.com>
2019-05-20 01:57:43 -0400

    Replaced x and y with position vector Added a tick method for gameloop purposes

--- src/asteroids/gameobject/GameObject.java
+++ src/asteroids/gameobject/GameObject.java
@@ -6,31 +6,31 @@
 package asteroids.gameobject;
 
 import asteroids.Asteroids;
-import java.awt.Graphics;
+import asteroids.Vector2D;
 import java.awt.Graphics2D;
 
 public abstract class GameObject {
 
     protected final Asteroids asteroids;
-    public int x;
-    public int y;
+    public Vector2D p;
     public double xSpeed;
     public double ySpeed;
 
-    public GameObject(Asteroids asteroids, int x, int y, double xSpeed, double ySpeed) {
+    public GameObject(Asteroids asteroids, Vector2D p, double xSpeed, double ySpeed) {
         this.asteroids = asteroids;
-        this.x = x;
-        this.y = y;
+        this.p = p;
         this.xSpeed = xSpeed;
         this.ySpeed = ySpeed;
     }
 
-    public int getX() {
-        return x;
+    public abstract void draw(Graphics2D graphics2D);
+
+    public double getX() {
+        return p.x;
     }
 
-    public int getY() {
-        return y;
+    public double getY() {
+        return p.y;
     }
 
     public double getXSpeed() {
@@ -41,5 +41,6 @@ public abstract class GameObject {
         return ySpeed;
     }
 
-    public abstract void draw(Graphics2D graphics2D);
+    public abstract void tick();
+
 }
