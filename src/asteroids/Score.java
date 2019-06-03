7046e03f6a09c2954590d98fa2fbd9b1776b7ac1
Farhan Faruqui <farhanfaruqui13@gmail.com>
2019-06-03 02:23:31 -0400

    Score object has int value variable Added custom font Draws score in the top rightcorner based on height and width of score Drawing score causes flickering *sad noises*

---
+++ src/asteroids/Score.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package asteroids;
+
+import asteroids.Asteroids;
+import java.awt.Font;
+import java.awt.FontFormatException;
+import java.awt.Graphics2D;
+import java.io.File;
+import java.io.IOException;
+
+public class Score {
+
+    protected final Asteroids asteroids;
+    private int value = 0;
+    private Font f;
+
+    public Score(Asteroids asteroids) {
+        this.asteroids = asteroids;
+        try {
+            f = Font.createFont(Font.PLAIN, new File(System.getProperty("user.dir") + "\\Fonts\\Score.ttf")).deriveFont(25f);
+        } catch (FontFormatException ex) {
+            System.out.println("Font not valid");
+        } catch (IOException ex) {
+            System.out.println("Font not found");
+        }
+    }
+
+    public void add(int x) {
+        value += x;
+    }
+
+    public void draw(Graphics2D graphics2D) {
+        asteroids.setFont(f);
+        graphics2D.drawString(String.valueOf(value), (int) (1100 - graphics2D.getFontMetrics(f).getStringBounds(String.valueOf(value), graphics2D).getWidth()), (int) (5 + graphics2D.getFontMetrics(f).getHeight()));
+    }
+
+    public void tick() {
+
+    }
+
+}
