b440c748d6674ec2ffe2beedd5e18ef3d5939e12
Farhan Faruqui <farhanfaruqui13@gmail.com>
2019-06-05 09:05:35 -0400

    Random stuff I cant remember like graphics

--- src/asteroids/gameobject/PowerUp.java
+++ src/asteroids/gameobject/PowerUp.java
@@ -1,23 +1,35 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package asteroids.gameobject;
 
 import asteroids.Asteroids;
 import asteroids.Vector2D;
 import java.awt.Graphics2D;
 import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 
 public class PowerUp extends GameObject {
 
-    public PowerUp(Asteroids asteroids, Vector2D p, Vector2D v) {
+    BufferedImage image = null;
+    String type = "";
+
+    public PowerUp(Asteroids asteroids, Vector2D p, Vector2D v, String type) {
         super(asteroids, p, v);
+        this.type = type;
+        try {
+            image = ImageIO.read(new File(System.getProperty("user.dir") + "\\Graphics\\Power Ups\\" + type + ".png"));
+        } catch (IOException ex) {
+        }
     }
 
     @Override
     public void draw(Graphics2D graphics2D) {
+        graphics2D.drawImage(image, (int) p.x, (int) p.y, asteroids);
+    }
+
+    public String getType() {
+        return type;
     }
 
     @Override
@@ -25,6 +37,6 @@ public class PowerUp extends GameObject {
     }
 
     public Rectangle getBounds() {
-        return null;
+        return new Rectangle((int) p.x - image.getWidth() / 2, (int) p.y - image.getHeight() / 2, image.getWidth(), image.getHeight());
     }
 }
