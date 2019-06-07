b5b8ce9c3f205c96d78827100e157359610005b6
sanjeev2001 <35245440+sanjeev2001@users.noreply.github.com>
2019-06-07 11:59:04 -0400

    Update KeyboardMovement.java

--- src/asteroids/KeyboardMovement.java
+++ src/asteroids/KeyboardMovement.java
@@ -14,31 +14,31 @@ public class KeyboardMovement extends KeyAdapter {
     private static boolean[] lastKeys = new boolean[256];
 
     public static boolean isDown(int key) {
-        return keys[key];
+        return keys[key];//checks if the key is being clicked or pressed down
     }
 
     @Override
     public void keyPressed(KeyEvent e) {
-        keys[e.getKeyCode()] = true;
+        keys[e.getKeyCode()] = true;//checks if a key is clicked
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
-        keys[e.getKeyCode()] = false;
+        keys[e.getKeyCode()] = false;//checks if key is released
     }
 
     public static void update() {
         for (int i = 0; i < 256; i++) {
-            lastKeys[i] = keys[i];
+            lastKeys[i] = keys[i];//updates what keys werenpressed
         }
     }
 
     public static boolean wasReleased(int key) {
-        return !isDown(key) && lastKeys[key];
+        return !isDown(key) && lastKeys[key];//allows for action to occur if these conditions are met
     }
 
     public static boolean wasPressed(int key) {
-        return isDown(key) && !lastKeys[key];
+        return isDown(key) && !lastKeys[key];//allows for action to occur if these conditions are met
     }
 
 }
